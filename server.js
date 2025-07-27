6b26858542271618aafe7fca49343abc386738e2
WendsonMagalhaes <wendson.silva@ccc.ufcg.edu.br>
2025-07-27 09:23:35 -0300

    Habilitando  CORS para frontend

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const produtoZeRoutes = require('./routes/produtoZeRoutes');
 const produtoIfoodRoutes = require('./routes/produtoIfoodRoutes');
+const cors = require('cors');
+
 
 dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/ze-delivery', produtoZeRoutes);
 app.use('/ifood', produtoIfoodRoutes);
